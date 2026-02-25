cbd5aa602dfd647504f9516520ad235a4e2a64f7
nweyinaye-11051997 <nweyinaye19970511@gmail.com>
2026-02-24 23:48:10 -0800

    'upload'

---
+++ angular/src/app/theme/layout/admin/navigation/navigation.ts
@@ -0,0 +1,287 @@
+export interface NavigationItem {
+  id: string;
+  title: string;
+  type: 'item' | 'collapse' | 'group';
+  translate?: string;
+  icon?: string;
+  hidden?: boolean;
+  url?: string;
+  classes?: string;
+  exactMatch?: boolean;
+  external?: boolean;
+  target?: boolean;
+  breadcrumbs?: boolean;
+  badge?: {
+    title?: string;
+    type?: string;
+  };
+  children?: NavigationItem[];
+}
+
+export const NavigationItems: NavigationItem[] = [
+  {
+    id: 'navigation',
+    title: 'Navigation',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'item',
+        url: '/analytics',
+        icon: 'feather icon-home'
+      }
+    ]
+  },
+    {
+    id: 'employee',
+    title: 'Employee',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'employeeinfo',
+        title: 'Employee Info',
+        type: 'item',
+        url: '/analytics',
+        icon: 'feather icon-home'
+      }
+    ]
+  }, {
+    id: 'attendance',
+    title: 'Attendance',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'attendanceupload',
+        title: 'Upload',
+        type: 'item',
+        url: '/analytics',
+        icon: 'feather icon-home'
+      },{
+        id: 'attendancecalculation',
+        title: 'Calculation',
+        type: 'item',
+        url: '/analytics',
+        icon: 'feather icon-home'
+      }
+    ]
+  },
+    {
+    id: 'setting-component',
+    title: 'Setting',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'basic',
+        title: 'Set up',
+        type: 'collapse',
+        icon: 'feather icon-box',
+        children: [
+          {
+            id: 'user',
+            title: 'User Setup',
+            type: 'item',
+            url: '/component/button'
+          },
+           {
+            id: 'branch',
+            title: 'Branch',
+            type: 'item',
+            url: '/component/button'
+          },
+           {
+            id: 'department',
+            title: 'Department ',
+            type: 'item',
+            url: '/component/button'
+          },{
+            id: 'designation',
+            title: 'Designation',
+            type: 'item',
+            url: '/component/button'
+          },
+           {
+            id: 'shift',
+            title: 'Shift',
+            type: 'item',
+            url: '/component/button'
+          },
+        ]
+        },
+    ]
+    },
+  {
+    id: 'ui-component',
+    title: 'Ui Component',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'basic',
+        title: 'Component',
+        type: 'collapse',
+        icon: 'feather icon-box',
+        children: [
+          {
+            id: 'button',
+            title: 'Button',
+            type: 'item',
+            url: '/component/button'
+          },
+          {
+            id: 'badges',
+            title: 'Badges',
+            type: 'item',
+            url: '/component/badges'
+          },
+          {
+            id: 'breadcrumb-pagination',
+            title: 'Breadcrumb & Pagination',
+            type: 'item',
+            url: '/component/breadcrumb-paging'
+          },
+          {
+            id: 'collapse',
+            title: 'Collapse',
+            type: 'item',
+            url: '/component/collapse'
+          },
+          {
+            id: 'tabs-pills',
+            title: 'Tabs & Pills',
+            type: 'item',
+            url: '/component/tabs-pills'
+          },
+          {
+            id: 'typography',
+            title: 'Typography',
+            type: 'item',
+            url: '/component/typography'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 'Authentication',
+    title: 'Authentication',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'signup',
+        title: 'Sign up',
+        type: 'item',
+        url: '/register',
+        icon: 'feather icon-at-sign',
+        target: true,
+        breadcrumbs: false
+      },
+      {
+        id: 'signin',
+        title: 'Sign in',
+        type: 'item',
+        url: '/login',
+        icon: 'feather icon-log-in',
+        target: true,
+        breadcrumbs: false
+      }
+    ]
+  },
+  {
+    id: 'chart',
+    title: 'Chart',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'apexchart',
+        title: 'ApexChart',
+        type: 'item',
+        url: '/chart',
+        classes: 'nav-item',
+        icon: 'feather icon-pie-chart'
+      }
+    ]
+  },
+  {
+    id: 'forms & tables',
+    title: 'Forms & Tables',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'forms',
+        title: 'Basic Forms',
+        type: 'item',
+        url: '/forms',
+        classes: 'nav-item',
+        icon: 'feather icon-file-text'
+      },
+      {
+        id: 'tables',
+        title: 'Tables',
+        type: 'item',
+        url: '/tables',
+        classes: 'nav-item',
+        icon: 'feather icon-server'
+      }
+    ]
+  },
+  {
+    id: 'other',
+    title: 'Other',
+    type: 'group',
+    icon: 'icon-group',
+    children: [
+      {
+        id: 'sample-page',
+        title: 'Sample Page',
+        type: 'item',
+        url: '/sample-page',
+        classes: 'nav-item',
+        icon: 'feather icon-sidebar'
+      },
+      {
+        id: 'menu-level',
+        title: 'Menu Levels',
+        type: 'collapse',
+        icon: 'feather icon-menu',
+        children: [
+          {
+            id: 'menu-level-2.1',
+            title: 'Menu Level 2.1',
+            type: 'item',
+            url: 'javascript:',
+            external: true
+          },
+          {
+            id: 'menu-level-2.2',
+            title: 'Menu Level 2.2',
+            type: 'collapse',
+            children: [
+              {
+                id: 'menu-level-2.2.1',
+                title: 'Menu Level 2.2.1',
+                type: 'item',
+                url: 'javascript:',
+                external: true
+              },
+              {
+                id: 'menu-level-2.2.2',
+                title: 'Menu Level 2.2.2',
+                type: 'item',
+                url: 'javascript:',
+                external: true
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
